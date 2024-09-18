0be3cdfe80897d3c8283426e5a894992e8a2eed3
cleblond <cleblond@iup.edu>
2024-09-18 07:37:38 -0400

    update newman

--- app.js
+++ app.js
@@ -138,6 +138,76 @@ function drawBonds(ctx, cx, cy, bondLength, substituents, rotation, isFront = fa
     }
 }
 
+
+function calcTextAlign(x,y) {
+    //1st quad
+    if (Math.round(x) == 0 && y > 150) {
+        console.log("Here");
+        return 'left';
+    }
+
+    if (Math.round(x) == 0 && y < 150) {
+        console.log("Here");
+        return 'left';
+    }
+
+
+    if (x > 150 && y < 150) {
+        console.log("Here");
+        return 'left';
+    }
+    
+    if (x < 150 && y < 150) {
+        console.log("Here");
+        return 'right';
+    }
+
+    if (x < 150 && y > 150) {
+        console.log("Here");
+        return 'right';
+    }
+
+
+
+}
+
+function calcTextBaseLine(x,y) {
+
+    if (Math.round(x) == 0 && y > 150) {
+        console.log("Here");
+        return 'bottom';
+    }
+
+    if (Math.round(x) == 0 && y < 150) {
+        console.log("Here");
+        return 'top';
+    }
+
+    if (x > 150 && y > 150) {
+        console.log("Here");
+        return 'middle';
+    }
+
+
+    if (x > 150 && y < 150) {
+        console.log("Here");
+        return 'middle';
+    }
+    
+    if (x < 150 && y < 150) {
+        console.log("Here");
+        return 'right';
+    }
+
+    if (x < 150 && y > 150) {
+        console.log("Here");
+        return 'right';
+    }
+
+}
+
+
+
 // Function to draw partial bond that is visible outside the circle
 function drawPartialBond(ctx, cx, cy, xEnd, yEnd, circleRadius) {
     // Calculate intersection points with the circle
